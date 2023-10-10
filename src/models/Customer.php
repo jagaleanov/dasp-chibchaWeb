974cb23b4b24d0fe065c0c74aa0d36f33fada260
jagaleanov <jagaleanov@udistrital.edu.co>
2023-10-10 13:59:06 -0500

    add updated_at to customer model

--- src/models/Customer.php
+++ src/models/Customer.php
@@ -14,6 +14,7 @@ class Customer {
         $this->id = $id;
         $this->name = $name;
         $this->email = $email;
+        $this->created_at = $created_at;
         $this->updated_at = $updated_at;
     }
 }
